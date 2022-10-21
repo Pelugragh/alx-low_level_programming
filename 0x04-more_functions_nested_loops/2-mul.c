9dccbd4ec9af908ff112fb61195bf10aff57545b
Pelugraph <emmanuelamoo765@gmail.com>
2022-10-20 21:05:36 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main"
+
+/**
+ * mul - Multiplies two integers.
+ * @a: is the first integer
+ * @b: is the second integer
+ * Return: addition of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
